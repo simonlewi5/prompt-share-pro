40f26263e75357dd0ac4869e308807720882663d
Simon Lewis <lewissimonjohn@gmail.com>
2024-10-20 11:57:40 -0700

    added api. in wsgi.py

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,4 +1,9 @@
-from app import app
+"""
+Load the Flask app and run it
+This is the entry point for the Flask app using WSGI
+"""
+
+from api.app import app
 
 if __name__ == "__main__":
     app.run()
